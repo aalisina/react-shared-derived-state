bf9f7094f39ce0644932838fd0570bec31f39440
sinazay123 <sinazay123@gmail.com>
2021-06-26 19:51:25 -0500

    Added funtionality to display the number of items in the cart or show that it's empty

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -42,9 +42,12 @@ export default function Cart({ cart, updateQuantity }) {
   if (loading) return <Spinner />;
   if (error) throw error;
 
+  const numItemsInCart = cart.reduce((prevValue, currentValue)=> prevValue + currentValue.quantity, 0)
+
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1>{numItemsInCart === 0 ? "Your cart is empty" :
+      `${numItemsInCart} item${numItemsInCart > 1 ? "s" : ""} in my cart`}</h1>
       <ul>{cart.map(renderItem)}</ul>
     </section>
   );
